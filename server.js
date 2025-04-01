53e3341c560f0eb113bd8563e5600b8cb0a1decf
charlie kelly <charlie.kelly@elastic.co>
2025-04-01 18:57:14 +0100

    fix for authing

--- server.js
+++ server.js
@@ -7,21 +7,23 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const client = new Client({
-  node: 'http://localhost:9200',
-  auth: {
-    username: 'elastic',
-    password: 'changeme'
-  }
-});
+
 
 app.post('/api/location', async (req, res) => {
+  const client = new Client({
+    node: 'http://localhost:9200',
+    auth: {
+      username: req.body.username,
+      password: req.body.password
+    }
+  });
   try {
     const response = await client.index({
       index: 'locations',
       document: req.body
     });
     res.status(200).json({ result: response.result });
+    console.error('Indexing', req.body.username);
   } catch (error) {
     console.error('Indexing error:', error.meta?.body || error.message);
     res.status(500).json({ error: 'Elasticsearch indexing failed' });
